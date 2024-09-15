302dca753a37a3dfd635e1c7609212161c7d04ab
Mohammad Ashik <mohammadashik2002@gmail.com>
2024-09-15 21:53:25 +0600

    for & while Loop

---
+++ m4p1.js
@@ -0,0 +1,19 @@
+// for loop
+// var numbers = [0, 5, 20, 30, 50, 70, 90];
+
+// for (let number of numbers) {
+//   console.log(number);
+// }
+
+// for (let x = 1; x <= 60; x++) {
+//   console.log(
+//     "I will invest at least 6 hrs every single day for next 60 days!",
+//     x
+//   );
+// }
+
+// let y = 1;
+// while (y <= 60) {
+//   console.log("i am Learing", y);
+//   y++;
+// }
---
+++ m4p2.js
@@ -0,0 +1,46 @@
+// /**
+//  * its solve by for loop
+//  * /
+
+// for (let x = 1; x <= 60; x++) {
+//   console.log(
+//     "I will invest at least 6 hrs every single day for next 60 days!",
+//     x
+//   );
+// }
+
+// for (let num = 61; num <= 100; num++) {
+//   if (num % 2 !== 0) {
+//     console.log("Odd Num:", num);
+//   } else {
+//     console.log("Even Num:", num);
+//   }
+// }
+
+// let sum = 0;
+// for (z = 91; z <= 129; z++) {
+//   if (z % 2 === 1) {
+//     console.log((sum = sum + z));
+//   }
+// }
+
+// let sum = 0;
+
+// for (let x = 51; x <= 85; x++) {
+//   if (x % 2 === 0) {
+//     console.log((sum = sum + x));
+//   }
+// }
+
+// /**
+//  * its solve by while loop
+//  * /
+
+// let x = 1;
+// while (x <= 60) {
+//   console.log(
+//     "I will invest at least 6 hrs every single day for next 60 days!",
+//     x
+//   );
+//   x++;
+// }
