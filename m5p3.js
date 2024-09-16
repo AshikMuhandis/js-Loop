502aa22c3c951ca71661d33a6ebafc2a38328cc5
Mohammad Ashik <mohammadashik2002@gmail.com>
2024-09-16 22:06:23 +0600

    Object Practice

---
+++ m5p3.js
@@ -0,0 +1,47 @@
+// let n = "Bangladesh";
+
+// console.log(n.includes(g));
+
+// // let m = n.slice(0, 5);
+// // console.log(m);
+
+// const sentence = "Hello, world!";
+// const word = "world";
+
+// const hh = sentence.includes(Hello); // true or false
+// console.log(hh);
+
+// let ss = "Rahim, Kahim , Sahim, Lahim";
+// let s = ss.split("/");
+// console.log("n1", ss);
+// console.log("n2", s);
+
+// let friends = ["Rahim", "Kahim", "sahim", "nahim", "lahim"];
+// // console.log(friends);
+// console.log(friends.join(" / "));
+
+// let fname = "arnob";
+// let lname = "ashik";
+// // let full = fname + " " + lname;
+// let full = fname.concat(" ").concat(lname);
+// console.log(full);
+
+// let word = "i am Programmer";
+// let rev = "";
+
+// for (const letter of me) {
+//   rev = letter + rev;
+// }
+// console.log(rev);
+
+// let word = "i am Programmer";
+// let rev = "";
+// for (let i = 0; i < word.length; i++) {
+//   let l = word[i];
+//   rev = l + rev;
+// }
+// console.log(rev);
+
+// let word = "i am Programmer";
+// let rev = word.split("").reverse().join("");
+// console.log(rev);
---
+++ m5p5.js
@@ -0,0 +1,34 @@
+let mobile = {
+  brand: "Nokia",
+  price: 14000,
+  buyDate: "05/05/24",
+  itsClean: true,
+  useDaly: false,
+  fetures: {
+    camera: {
+      front: "16M",
+      back: "24M",
+    },
+    display: "6.5'",
+    finger: true,
+    battary: "fixed",
+  },
+  sellit: "insha-allah",
+};
+// mobile.pri = "LG";
+// let naam = mobile.brand;
+
+// mobile.brand = ["noakia", "Komia"];
+
+// const date = "buyDate";
+// console.log(mobile["useDaly"]);
+
+// let brandCng = "brand";
+// mobile[brandCng] = "Realmi";
+
+// console.log(mobile);
+// const kay = Object.keys(mobile);
+// console.log(kay);
+
+// let values = Object.values(mobile);
+// console.log(values);
